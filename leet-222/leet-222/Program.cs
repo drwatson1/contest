75ce712554c948490a27f579e02996ef3c63115b
Сергей Трегуб <sergey.tregub@vocord.ru>
2022-11-28 22:34:00 +0300

    Improved version of leet-222

--- leet-222/leet-222/Program.cs
+++ leet-222/leet-222/Program.cs
@@ -2,40 +2,30 @@
 
 int CountNodes(TreeNode root)
 {
-    int height(TreeNode root, Func<TreeNode, TreeNode> get_node)
-    {
-        TreeNode cur_node = root;
-        int count = 0;
-        while (cur_node != null)
-        {
-            count++;
-            cur_node = get_node(cur_node);
-        }
-        return count;
-    }
-
     if ( root == null)
     {
         return 0;
     }
 
-    int left_height = height(root.left, x => x.left);
-    int right_height = height(root.right, x => x.right);
-
-    if (left_height == right_height)
+    int lh = 1;
+    var left = root;
+    while ( (left = left.left) != null)
     {
-        return (int)Math.Pow(2, left_height + 1) - 1;
+        ++lh;
+    }
+    int rh = 1;
+    var right = root;
+    while( (right = right.right) != null)
+    {
+        ++rh;
+    }
+
+    if (lh == rh)
+    {
+        return  (1<< lh) - 1;
     }
     else
     {
-        int left_count = CountNodes(root.left);
-        int right_count = CountNodes(root.right);
-
-        return left_count + right_count + 1;
+        return CountNodes(root.left) + CountNodes(root.right) + 1;
     }
 }
-
-TreeNode root = null;
-int expected = 0;
-int result;
-
